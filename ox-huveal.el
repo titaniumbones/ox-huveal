087bff79ea86810a8cd85dfa7a3049490345a3f3
Matt Price <matt.price@utoronto.ca>
2017-09-15 07:20:31 -0400

    initial implementation of final-export-filter
    Use a primitive regexp replacement filter instead of rewriting
    template and css/js linking functions.  It may be too greedy,
    unfortunately, so not merging for now.

--- ox-huveal.el
+++ ox-huveal.el
@@ -90,8 +90,9 @@
 ;;;; links (images esp)
 (defun org-huveal-link (link desc info)
   "Transcode a LINK object from Org to Reveal. The result is
-  identical to ox-html expect for image links. When `org-reveal-single-file' is t,
-the result is the Data URIs of the referenced image."
+  identical to ox-html expect for image links. unlike `org-huveal-link, 
+ `org-reveal-single-file' has no effect. This is because the presentations are 
+intended to be viewed in the inline environment."
     (message "link type is %s" (org-element-property :type link))
   (let* ((local-image-p (and (string= "file" (org-element-property :type link))
                              (org-export-inline-image-p
@@ -106,8 +107,8 @@ the result is the Data URIs of the referenced image."
          ;; (can-embed-image (and want-embed-image
          ;;                       (file-readable-p clean-path)))
          )
-    (message "ox-huveal: got past the let in huveal-link")
-    (message "org-element-porpoerty %s" raw-path )
+    ;; (message "ox-huveal: got past the let in huveal-link")
+    ;; (message "org-element-porpoerty %s" raw-path )
     (if local-image-p
         (let ((path (org-hugo--attachment-rewrite-maybe
                      (if (file-name-absolute-p raw-path)
@@ -144,148 +145,143 @@ the result is the Data URIs of the referenced image."
     ))
 
 ;;;; label css
-(defun org-huveal--css-label (in-single-file file-name style-id)
-  "Generate an HTML label for including a CSS file, if
-  IN-SINGLE-FILE is t, the content of FILE-NAME is embedded,
-  otherwise, a `<link>' label is generated."
+;;;; commented out for now!
+;; (defun org-huveal--css-label (in-single-file file-name style-id)
+;;   "Generate an HTML label for including a CSS file, if
+;;   IN-SINGLE-FILE is t, the content of FILE-NAME is embedded,
+;;   otherwise, a `<link>' label is generated."
 
-  (message "in org-huveal--css-label with %s" file-name)
-  ;; (when (and file-name (not (string= file-name "")))
-  ;;   (if in-single-file
-  ;;       ;; Single-file
-  ;;       (let ((local-file-name (org-reveal--file-url-to-path file-name)))
-  ;;         (message "ox-reveal: in single file, how did that happen?")
-  ;;         (if (file-readable-p local-file-name)
-  ;;             (concat "<style type=\"text/css\">\n"
-  ;;                     (org-reveal--read-file local-file-name)
-  ;;                     "\n</style>\n")
-  ;;           ;; But file is not readable.
-  ;;           (error "Cannot read %s" file-name)))
-  ;;     ;; Not in-single-file
-  ;;     ))
-  (concat "<link rel=\"stylesheet\" href=\"" file-name "\""
-          (if style-id  (format " id=\"%s\"" style-id))
-          "/>\n"))
-;;;; Stylesheets
-(defun org-huveal-stylesheets (info)
-  "Return the HTML contents for declaring reveal stylesheets
-using custom variable `org-reveal-root'."
-  (message "in org-huveal-style")
-  (let* ((root-path (file-name-as-directory (plist-get info :reveal-root)))
-         (reveal-css (concat root-path "css/reveal.css"))
-         (theme (plist-get info :reveal-theme))
-         (theme-css (concat root-path "css/theme/" theme ".css"))
-         (extra-css (plist-get info :reveal-extra-css))
-          (in-single-file (plist-get info :reveal-single-file))
-         )
-    (message "past initial let*")
-    (concat
-     ;; Default embedded style sheets
-     "<style type=\"text/css\">
-.underline { text-decoration: underline; }
-</style>
-"
-     ;; stylesheets
-     (mapconcat (lambda (elem) (org-huveal--css-label nil (car elem) (cdr elem)))
-                (append (list (cons reveal-css nil)
-                              (cons theme-css "theme"))
-                        (mapcar (lambda (a) (cons a nil))
-                                (split-string extra-css "\n")))
-                "\n")
+;;   (message "in org-huveal--css-label with %s" file-name)
+;;   ;; (when (and file-name (not (string= file-name "")))
+;;   ;;   (if in-single-file
+;;   ;;       ;; Single-file
+;;   ;;       (let ((local-file-name (org-reveal--file-url-to-path file-name)))
+;;   ;;         (message "ox-reveal: in single file, how did that happen?")
+;;   ;;         (if (file-readable-p local-file-name)
+;;   ;;             (concat "<style type=\"text/css\">\n"
+;;   ;;                     (org-reveal--read-file local-file-name)
+;;   ;;                     "\n</style>\n")
+;;   ;;           ;; But file is not readable.
+;;   ;;           (error "Cannot read %s" file-name)))
+;;   ;;     ;; Not in-single-file
+;;   ;;     ))
+;;   (concat "<link rel=\"stylesheet\" href=\"" file-name "\""
+;;           (if style-id  (format " id=\"%s\"" style-id))
+;;           "/>\n"))
+;; ;;;; Stylesheets
+;; (defun org-huveal-stylesheets (info)
+;;   "Return the HTML contents for declaring reveal stylesheets
+;; using custom variable `org-reveal-root'."
+;;   (message "in org-huveal-style")
+;;   (let* ((root-path (file-name-as-directory (plist-get info :reveal-root)))
+;;          (reveal-css (concat root-path "css/reveal.css"))
+;;          (theme (plist-get info :reveal-theme))
+;;          (theme-css (concat root-path "css/theme/" theme ".css"))
+;;          (extra-css (plist-get info :reveal-extra-css))
+;;           (in-single-file (plist-get info :reveal-single-file))
+;;          )
+;;     (message "past initial let*")
+;;     (concat
+;;      ;; Default embedded style sheets
+;;      "<style type=\"text/css\">
+;; .underline { text-decoration: underline; }
+;; </style>
+;; "
+;;      ;; stylesheets
+;;      (mapconcat (lambda (elem) (org-huveal--css-label nil (car elem) (cdr elem)))
+;;                 (append (list (cons reveal-css nil)
+;;                               (cons theme-css "theme"))
+;;                         (mapcar (lambda (a) (cons a nil))
+;;                                 (split-string extra-css "\n")))
+;;                 "\n")
 
-     ;; Include CSS for highlight.js if necessary
-     (if (org-reveal--using-highlight.js info)
-         (format "<link rel=\"stylesheet\" href=\"%s\"/>"
-                 (format-spec (plist-get info :reveal-highlight-css)
-                              `((?r . ,(directory-file-name root-path))))))
-     ;; print-pdf
-     (if in-single-file ""
-       (format "
-<!-- If the query includes 'print-pdf', include the PDF print sheet -->
-<script>
-    if( window.location.search.match( /print-pdf/gi ) ) {
-        var link = document.createElement( 'link' );
-        link.rel = 'stylesheet';
-        link.type = 'text/css';
-        link.href = '%scss/print/pdf.css';
-        document.getElementsByTagName( 'head' )[0].appendChild( link );
-    }
-</script>
-"
-               root-path)))))
+;;      ;; Include CSS for highlight.js if necessary
+;;      (if (org-reveal--using-highlight.js info)
+;;          (format "<link rel=\"stylesheet\" href=\"%s\"/>"
+;;                  (format-spec (plist-get info :reveal-highlight-css)
+;;                               `((?r . ,(directory-file-name root-path))))))
+;;      ;; print-pdf
+;;      (if in-single-file ""
+;;        (format "
+;; <!-- If the query includes 'print-pdf', include the PDF print sheet -->
+;; <script>
+;;     if( window.location.search.match( /print-pdf/gi ) ) {
+;;         var link = document.createElement( 'link' );
+;;         link.rel = 'stylesheet';
+;;         link.type = 'text/css';
+;;         link.href = '%scss/print/pdf.css';
+;;         document.getElementsByTagName( 'head' )[0].appendChild( link );
+;;     }
+;; </script>
+;; "
+;;                root-path)))))
 
-;;;;; Template
-(defun org-huveal-template (contents info)
-  "Return complete document string after HTML conversion.
-contents is the transcoded contents string.
-info is a plist holding export options."
-  (message "in huveal-template")
-  (concat
-   (format "<!DOCTYPE html>\n<html%s>\n<head>\n"
-           (if-format " lang=\"%s\"" (plist-get info :language)))
-   "<meta charset=\"utf-8\"/>\n"
-   (if-format "<title>%s</title>\n" (org-export-data (plist-get info :title) info))
-   (if-format "<meta name=\"author\" content=\"%s\"/>\n" (plist-get info :author))
-   (if-format "<meta name=\"description\" content=\"%s\"/>\n" (plist-get info :description))
-   (if-format "<meta name=\"keywords\" content=\"%s\"/>\n" (plist-get info :keywords))
-   (org-huveal-stylesheets info)
-   (org-reveal-mathjax-scripts info)
-   (org-reveal--build-pre/postamble 'head-preamble info)
-   (org-element-normalize-string (plist-get info :html-head))
-   (org-element-normalize-string (plist-get info :html-head-extra))
-   "</head>
-<body>\n"
-   (org-reveal--build-pre/postamble 'preamble info)
-   "<div class=\"reveal\">
-<div class=\"slides\">\n"
-   ;; Title slides
-   (let ((title-slide (plist-get info :reveal-title-slide)))
-     (when (and title-slide (not (plist-get info :reveal-subtree)))
-       (let ((title-slide-background (plist-get info :reveal-title-slide-background))
-             (title-slide-background-size (plist-get info :reveal-title-slide-background-size))
-             (title-slide-background-position (plist-get info :reveal-title-slide-background-position))
-             (title-slide-background-repeat (plist-get info :reveal-title-slide-background-repeat))
-             (title-slide-background-transition (plist-get info :reveal-title-slide-background-transition))
-             (title-slide-with-header (plist-get info :reveal-slide-global-header))
-             (title-slide-with-footer (plist-get info :reveal-slide-global-footer)))
-         (concat "<section id=\"sec-title-slide\""
-                 (when title-slide-background
-                   (concat " data-background=\"" title-slide-background "\""))
-                 (when title-slide-background-size
-                   (concat " data-background-size=\"" title-slide-background-size "\""))
-                 (when title-slide-background-position
-                   (concat " data-background-position=\"" title-slide-background-position "\""))
-                 (when title-slide-background-repeat
-                   (concat " data-background-repeat=\"" title-slide-background-repeat "\""))
-                 (when title-slide-background-transition
-                   (concat " data-background-transition=\"" title-slide-background-transition "\""))
-                 ">"
-                 (when title-slide-with-header
-                   (let ((header (plist-get info :reveal-slide-header)))
-                     (when header (format "<div class=\"slide-header\">%s</div>\n" header))))
-                 (cond ((eq title-slide nil) nil)
-                       ((stringp title-slide) (format-spec title-slide (org-html-format-spec info)))
-                       ((eq title-slide 'auto) (org-reveal--auto-title-slide-template info)))
-                 "\n"
-                 (when title-slide-with-footer
-                   (let ((footer (plist-get info :reveal-slide-footer)))
-                     (when footer (format "<div class=\"slide-footer\">%s</div>\n" footer))))
-                 "</section>\n"))))
-   contents
-   "</div>
-</div>\n"
-   (org-reveal--build-pre/postamble 'postamble info)
-   (message "about to build scripts")
-   (org-huveal-scripts info)
-   (message "done with scripts")
-   "</body>
-</html>\n"))
-
-
-;;;; scripts
-
-
-;;;;; Helpers
+;; ;;;;; Template
+;; (defun org-huveal-template (contents info)
+;;   "Return complete document string after HTML conversion.
+;; contents is the transcoded contents string.
+;; info is a plist holding export options."
+;;   (message "in huveal-template")
+;;   (concat
+;;    (format "<!DOCTYPE html>\n<html%s>\n<head>\n"
+;;            (if-format " lang=\"%s\"" (plist-get info :language)))
+;;    "<meta charset=\"utf-8\"/>\n"
+;;    (if-format "<title>%s</title>\n" (org-export-data (plist-get info :title) info))
+;;    (if-format "<meta name=\"author\" content=\"%s\"/>\n" (plist-get info :author))
+;;    (if-format "<meta name=\"description\" content=\"%s\"/>\n" (plist-get info :description))
+;;    (if-format "<meta name=\"keywords\" content=\"%s\"/>\n" (plist-get info :keywords))
+;;    (org-huveal-stylesheets info)
+;;    (org-reveal-mathjax-scripts info)
+;;    (org-reveal--build-pre/postamble 'head-preamble info)
+;;    (org-element-normalize-string (plist-get info :html-head))
+;;    (org-element-normalize-string (plist-get info :html-head-extra))
+;;    "</head>
+;; <body>\n"
+;;    (org-reveal--build-pre/postamble 'preamble info)
+;;    "<div class=\"reveal\">
+;; <div class=\"slides\">\n"
+;;    ;; Title slides
+;;    (let ((title-slide (plist-get info :reveal-title-slide)))
+;;      (when (and title-slide (not (plist-get info :reveal-subtree)))
+;;        (let ((title-slide-background (plist-get info :reveal-title-slide-background))
+;;              (title-slide-background-size (plist-get info :reveal-title-slide-background-size))
+;;              (title-slide-background-position (plist-get info :reveal-title-slide-background-position))
+;;              (title-slide-background-repeat (plist-get info :reveal-title-slide-background-repeat))
+;;              (title-slide-background-transition (plist-get info :reveal-title-slide-background-transition))
+;;              (title-slide-with-header (plist-get info :reveal-slide-global-header))
+;;              (title-slide-with-footer (plist-get info :reveal-slide-global-footer)))
+;;          (concat "<section id=\"sec-title-slide\""
+;;                  (when title-slide-background
+;;                    (concat " data-background=\"" title-slide-background "\""))
+;;                  (when title-slide-background-size
+;;                    (concat " data-background-size=\"" title-slide-background-size "\""))
+;;                  (when title-slide-background-position
+;;                    (concat " data-background-position=\"" title-slide-background-position "\""))
+;;                  (when title-slide-background-repeat
+;;                    (concat " data-background-repeat=\"" title-slide-background-repeat "\""))
+;;                  (when title-slide-background-transition
+;;                    (concat " data-background-transition=\"" title-slide-background-transition "\""))
+;;                  ">"
+;;                  (when title-slide-with-header
+;;                    (let ((header (plist-get info :reveal-slide-header)))
+;;                      (when header (format "<div class=\"slide-header\">%s</div>\n" header))))
+;;                  (cond ((eq title-slide nil) nil)
+;;                        ((stringp title-slide) (format-spec title-slide (org-html-format-spec info)))
+;;                        ((eq title-slide 'auto) (org-reveal--auto-title-slide-template info)))
+;;                  "\n"
+;;                  (when title-slide-with-footer
+;;                    (let ((footer (plist-get info :reveal-slide-footer)))
+;;                      (when footer (format "<div class=\"slide-footer\">%s</div>\n" footer))))
+;;                  "</section>\n"))))
+;;    contents
+;;    "</div>
+;; </div>\n"
+;;    (org-reveal--build-pre/postamble 'postamble info)
+;;    (message "about to build scripts")
+;;    (org-huveal-scripts info)
+;;    (message "done with scripts")
+;;    "</body>
+;; </html>\n"))
 
 
 ;;;; export functions
@@ -366,13 +362,20 @@ Returns nil if a valid Hugo post subtree is not found."
 ;; experimental filter
 (defun org-huveal-final-filter (text backend info)
   "replaces org-reveal paths with org-huveal paths"
-  (let ((rev-root (plist-get info :org-reveal-root))
-        (huv-root (plist-get info :org-huveal-root))
-        (rev-extra-css (plist-get info :org-reveal-extra-css))
-        (huv-extra-css (plist-get info :org-huveal-extra-css))
-        (rev-extra-js (plist-get info :org-reveal-extra-js))
-        (huv-extra-js (plist-get info :org-huveal-extra-js)))
-    (message "ox-huveal, text is: %s" x)
+  (message "in org-huveal-final-filter w/ info= \n%s" info)
+  (let ((rev-root (plist-get info :reveal-root))
+        (huv-root (plist-get info :huveal-root))
+        (rev-extra-css (plist-get info :reveal-extra-css))
+        (huv-extra-css (plist-get info :huveal-extra-css))
+        (rev-extra-js (plist-get info :reveal-extra-js))
+        (huv-extra-js (plist-get info :huveal-extra-js)))
+    ;; (message "ox-huveal, variables:")
+    ;; (message "rev-root %s" rev-root)
+    ;; (message "huv-root %s" huv-root)
+    ;; (message "rev-css %s" rev-extra-css)
+    ;; (message "huv-css %s" huv-extra-css)
+    ;; (message "rev-root %s" rev-root)
+    ;; (message "rev-root %s" rev-root)
     (replace-regexp-in-string rev-root huv-root
                               (replace-regexp-in-string rev-extra-css huv-extra-css
                                                         (replace-regexp-in-string rev-extra-js huv-extra-js
